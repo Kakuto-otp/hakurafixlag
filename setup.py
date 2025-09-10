621aa1d426844d1ecd9c65f71f0c271a0b7b5428
Kakuto-otp <kl782530@gmail.com>
2025-09-10 19:35:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     description="Fix lag Android và gửi file Telegram tự động.",
     author="Kakuto-otp",
     packages=find_packages(),
+    package_dir={"hakurafixlag": "hakurafixlag"}, 
     install_requires=["pyTelegramBotAPI"],
-    py_modules=["cli"],
     entry_points={
         "console_scripts": [
-            "fixlag=cli:main"
+            "fixlag=hakurafixlag.cli:main"
         ]
     }
 )
